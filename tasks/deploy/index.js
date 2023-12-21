840cb5552eacdc0fd376d9dac764133e7e450a0c
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-12-22 01:05:12 +0530

    final commit

--- tasks/deploy/index.js
+++ tasks/deploy/index.js
@@ -9,12 +9,12 @@ exports.deployTestNFT = require("./deployTestNFT.js")
 
 /* Deploy order 
 
-1. ERC6551 Implementation - npx hardhat deploy-implementation --network polygonMumbai --verify true
-2. ERC6551 Registry - npx hardhat deploy-registry --network polygonMumbai --verify true
-3. Relationship Implementation - npx hardhat deploy-relationship --network polygonMumbai --verify true
-4. Relationship Registry - npx hardhat deploy-relationship-registry --network polygonMumbai --verify true
-5. InCraft - npx hardhat deploy-incraft --network polygonMumbai --verify true
-6. CraftToken - npx hardhat deploy-craft-token --network polygonMumbai --verify true
+1. ERC6551 Implementation - npx hardhat deploy-implementation --network inEvmTestnet --verify true
+2. ERC6551 Registry - npx hardhat deploy-registry --network inEvmTestnet --verify true
+3. Relationship Implementation - npx hardhat deploy-relationship --network inEvmTestnet --verify true
+4. Relationship Registry - npx hardhat deploy-relationship-registry --network inEvmTestnet --verify true
+5. InCraft - npx hardhat deploy-incraft --network inEvmTestnet --verify true
+6. CraftToken - npx hardhat deploy-craft-token --network inEvmTestnet --verify true
 
 Call setCraftToken in InCraft
 Call initialize in RelRegistry

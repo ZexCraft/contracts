9773fa6df24eca3dfca03170261f3d7f0612992c
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2023-12-27 16:06:09 +0530

    add mainnet deployments

--- tasks/deploy/index.js
+++ tasks/deploy/index.js
@@ -9,12 +9,12 @@ exports.deployTestNFT = require("./deployTestNFT.js")
 
 /* Deploy order 
 
-1. ERC6551 Implementation - npx hardhat deploy-implementation --network victionTestnet --verify true
-2. ERC6551 Registry - npx hardhat deploy-registry --network victionTestnet --verify true
-3. Relationship Implementation - npx hardhat deploy-relationship --network victionTestnet --verify true
-4. Relationship Registry - npx hardhat deploy-relationship-registry --network victionTestnet --verify true
-5. ZexCraft - npx hardhat deploy-zexcraft --network victionTestnet --verify true
-6. CraftToken - npx hardhat deploy-craft-token --network victionTestnet --verify true
+1. ERC6551 Implementation - npx hardhat deploy-implementation --network viction --verify true
+2. ERC6551 Registry - npx hardhat deploy-registry --network viction --verify true
+3. Relationship Implementation - npx hardhat deploy-relationship --network viction --verify true
+4. Relationship Registry - npx hardhat deploy-relationship-registry --network viction --verify true
+5. ZexCraft - npx hardhat deploy-zexcraft --network viction --verify true
+6. CraftToken - npx hardhat deploy-craft-token --network viction --verify true
 
 Call setCraftToken in ZexCraft
 Call initialize in RelRegistry
